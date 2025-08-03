58f287d383471b2936a4c82425949e5c17c4b580
Samael Home <samaelortiz2218@gmail.com>
2025-08-03 13:58:51 -0700

    Mostrando todas las clases en dashboard

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,7 +1,10 @@
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
-
+import {
+    Card,
+    CardContent,
+} from "@/components/ui/card"
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Dashboard',
@@ -9,22 +12,71 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-export default function Dashboard({clases, materias}: {clases: Array<{id: number, dia: string, profesor: string, salon: string, edificio: string, hora_inicio: string, hora_fin: string, materia_id: number}>, materias: Array<{id: number, nombre: string}>}) {
+export default function Dashboard({ clases, materias }: { clases: Array<{ id: number, dia: string, profesor: string, salon: string, edificio: string, hora_inicio: string, hora_fin: string, materia_id: number }>, materias: Array<{ id: number, nombre: string }> }) {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-                {clases.map((clase) => (
-                    <div key={clase.id}>
-                        <h2>{clase.dia}</h2>
-                        <p>{clase.profesor}</p>
-                        <p>{clase.salon}</p>
-                        <p>{clase.edificio}</p>
-                        <p>{clase.hora_inicio}</p>
-                        <p>{clase.hora_fin}</p>
-                        <p>{materias.find((materia) => materia.id === clase.materia_id)?.nombre}</p>
-                    </div>
-                ))}
+            <div className="flex w-full justify-center h-full flex-1 gap-4 rounded-xl p-4 overflow-x-auto">
+                <div className="flex flex-col gap-4 text-center">
+                    <h1>Lunes</h1>
+                    {clases.filter((clase) => clase.dia === 'Lunes').map((clase) => (
+                        <Card key={clase.id}>
+                            <CardContent>
+                                <p>{materias.find((materia) => materia.id === clase.materia_id)?.nombre}</p>
+                                <p>{clase.hora_inicio}</p>
+                                <p>{clase.hora_fin}</p>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+                <div className="flex flex-col gap-4 text-center">
+                    <h1>Martes</h1>
+                    {clases.filter((clase) => clase.dia === 'Martes').map((clase) => (
+                        <Card key={clase.id}>
+                            <CardContent>
+                                <p>{materias.find((materia) => materia.id === clase.materia_id)?.nombre}</p>
+                                <p>{clase.hora_inicio}</p>
+                                <p>{clase.hora_fin}</p>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+                <div className="flex flex-col gap-4 text-center">
+                    <h1>Miercoles</h1>
+                    {clases.filter((clase) => clase.dia === 'Miercoles').map((clase) => (
+                        <Card key={clase.id}>
+                            <CardContent>
+                                <p>{materias.find((materia) => materia.id === clase.materia_id)?.nombre}</p>
+                                <p>{clase.hora_inicio}</p>
+                                <p>{clase.hora_fin}</p>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+                <div className="flex flex-col gap-4 text-center">
+                    <h1>Jueves</h1>
+                    {clases.filter((clase) => clase.dia === 'Jueves').map((clase) => (
+                        <Card key={clase.id}>
+                            <CardContent>
+                                <p>{materias.find((materia) => materia.id === clase.materia_id)?.nombre}</p>
+                                <p>{clase.hora_inicio}</p>
+                                <p>{clase.hora_fin}</p>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+                <div className="flex flex-col gap-4 text-center">
+                    <h1>Viernes</h1>
+                    {clases.filter((clase) => clase.dia === 'Viernes').map((clase) => (
+                        <Card key={clase.id}>
+                            <CardContent>
+                                <p>{materias.find((materia) => materia.id === clase.materia_id)?.nombre}</p>
+                                <p>{clase.hora_inicio}</p>
+                                <p>{clase.hora_fin}</p>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
             </div>
         </AppLayout>
     );
